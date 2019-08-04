a126551198088d73001b5223d04727cc5fbb4a82
enosvazquez <enosvazquez@gmail.com>
2019-08-03 18:37:02 -0700

    from floats to strings

---
+++ floats.py
@@ -0,0 +1,12 @@
+float_1 = 0.25
+float_2 = 40.0
+
+product0 = int(float_1) * int(float_2)
+print product0
+product  =  float(float_1) * float(float_2)
+print product
+
+string_a_fy_it = "The product was " + str(product)
+# here the str will convert product as a string 
+print string_a_fy_it 
+
